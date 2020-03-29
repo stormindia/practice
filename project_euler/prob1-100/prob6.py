3805e752fd631dec06d2224323ade8a0aab3e6b3
stormindia <bajpaiharshit14@gmail.com>
2020-03-29 19:47:23 +0530

    practice repo

---
+++ project_euler/prob1-100/prob6.py
@@ -0,0 +1,9 @@
+#URL - https://projecteuler.net/problem=6
+
+sum_100 = 100*101/2
+
+sum_100 = sum_100*sum_100
+
+sum_100_square = 100*101*201/6
+
+print(sum_100 - sum_100_square)
---
+++ project_euler/prob1-100/prob7.py
@@ -0,0 +1,9 @@
+#URl - https://projecteuler.net/problem=7
+#2,3,5
+# i = 1
+# while(i <= 9999 ):
+#     num = 6*i + 1
+#     i += 1
+
+num = 6 * (10001 - 3) + 1
+print(num)
